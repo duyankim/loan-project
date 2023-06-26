1b3ce218b03ffa9711a18e08cdb693602a49f228
Duyan Kim <arbolkim@gmail.com>
2023-06-27 00:22:00 +0900

    feat: CounselRepository and CounselServiceImpl

---
+++ src/main/java/com/loan/service/CounselServiceImpl.java
@@ -0,0 +1,29 @@
+package com.loan.service;
+
+import com.loan.domain.Counsel;
+import com.loan.dto.counselDTO.Request;
+import com.loan.dto.counselDTO.Response;
+import com.loan.repository.CounselRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class CounselServiceImpl implements CounselService {
+
+    private final CounselRepository counselRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response create(Request request) {
+        Counsel counsel = modelMapper.map(request, Counsel.class);
+        counsel.setAppliedAt(LocalDateTime.now());
+
+        Counsel created = counselRepository.save(counsel);
+        return modelMapper.map(created, Response.class);
+    }
+}
