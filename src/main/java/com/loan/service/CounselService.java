1526829ff9f280b2b4d2c9cdab58f6e0850d52ad
Duyan Kim <arbolkim@gmail.com>
2023-06-28 00:29:00 +0900

    feat: 상담 조회 기능 Service, Controller

--- src/main/java/com/loan/service/CounselService.java
+++ src/main/java/com/loan/service/CounselService.java
@@ -4,5 +4,9 @@ import com.loan.dto.CounselDTO.*;
 
 public interface CounselService {
 
+    // 상담 등록 기능
     Response create (Request request);
+
+    // 상담 조회 기능
+    Response get(Long counselId);
 }
