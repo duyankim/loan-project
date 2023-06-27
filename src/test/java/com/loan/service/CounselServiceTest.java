2365df89cff3692024abfe380a4dbb41ff0d590a
Duyan Kim <arbolkim@gmail.com>
2023-06-28 00:29:07 +0900

    feat: 상담 조회 기능 Service test

--- src/test/java/com/loan/service/CounselServiceTest.java
+++ src/test/java/com/loan/service/CounselServiceTest.java
@@ -5,12 +5,17 @@ import static org.mockito.Mockito.when;
 
 import com.loan.domain.Counsel;
 import com.loan.dto.CounselDTO.*;
+import com.loan.exception.BaseException;
+import com.loan.exception.ResultType;
 import com.loan.repository.CounselRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class CounselServiceTest {
@@ -50,5 +55,29 @@ public class CounselServiceTest {
         Response actual = counselService.create(request);
 
         assertThat(actual.getName()).isSameAs(entity.getName());
+    }
+
+    @Test
+    void Should_ReturnResponseOfExistCounselEntity_When_RequestExistCounselId() {
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = counselService.get(findId);
+
+        assertThat(actual.getCounselId()).isSameAs(findId);
+    }
+
+    @Test
+    void Should_ThrowException_When_RequestNotExistCounselId() {
+        Long findId = 2L;
+
+        when(counselRepository.findById(findId)).thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        Assertions.assertThrows(BaseException.class, () -> counselService.get(findId));
     }
 }
